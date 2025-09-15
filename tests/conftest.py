1d8c6eec9ef0d795b174c7310f60b436e24b8011
ixHades <0n1yw01f2018@gmail.com>
2025-09-15 23:31:47 +0330

    Add initial files

---
+++ tests/conftest.py
@@ -0,0 +1,35 @@
+import os
+import pytest
+import asyncio
+from tortoise import Tortoise
+MODEL_MODULES = ["app.db.models.users"]
+
+
+@pytest.fixture(scope="session")
+def event_loop():
+    policy = asyncio.get_event_loop_policy()
+    loop = policy.new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def initialize_tests():
+    await Tortoise.init(
+            db_url="sqlite://:memory:",
+            modules={"models": MODEL_MODULES}
+    )
+
+    await Tortoise.generate_schemas()
+
+    yield
+
+    await Tortoise.close_connections()
+@pytest.fixture()
+async def test_user():
+    from app.db.models.users import Users
+    import random
+    user = await Users.create(user_id=random.randint(10000000,99999999))
+    return user
+
+
