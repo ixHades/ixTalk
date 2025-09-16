3f67a27e524090d47763a797c3e76d0ee6fa18f2
ixHades <0n1yw01f2018@gmail.com>
2025-09-16 12:12:27 +0330

    Ruff format #1

--- app/db/models/admins.py
+++ app/db/models/admins.py
@@ -1,6 +1,7 @@
 from tortoise.models import Model
 from tortoise import fields
 from app.db.enums import AdminRole
+from .warnings import WarningEntry
 
 
 class Admins(Model):
@@ -8,3 +9,10 @@ class Admins(Model):
     role = fields.CharEnumField(AdminRole)
     created_at = fields.DatetimeField(auto_now_add=True)
     updated_at = fields.DatetimeField(auto_now=True)
+    issued_warnings: fields.ReverseRelation["WarningEntry"]
+
+    class Meta:
+        table = "admins"
+
+    def __str__(self) -> str:
+        return f"Admin {self.admin_user_id} - {self.role}"
--- app/db/models/reports.py
+++ app/db/models/reports.py
@@ -1,8 +1,13 @@
 from tortoise.models import Model
 from tortoise import fields
 
+
 class Reports(Model):
     report_id = fields.IntField(primary_key=True)
-    message_code = fields.ForeignKeyField(model_name="models.Messages", to_field="message_code")
-    reporter_user_id = fields.ForeignKeyField(model_name="models.Users", to_field="user_id")
+    message_code = fields.ForeignKeyField(
+        model_name="models.Messages", to_field="message_code"
+    )
+    reporter_user_id = fields.ForeignKeyField(
+        model_name="models.Users", to_field="user_id"
+    )
     created_at = fields.DatetimeField(auto_now_add=True)
