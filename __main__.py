2fe80a9c018f74e397101adae5fa0a1e48bc1e6d
ixHades <0n1yw01f2018@gmail.com>
2025-09-16 11:39:40 +0330

    update .gitignore __main__.py pyproject.toml

--- __main__.py
+++ __main__.py
@@ -10,12 +10,15 @@ load_dotenv()
 API_ID = os.getenv("API_ID")
 API_HASH = os.getenv("API_HASH")
 TOKEN = os.getenv("BOT_TOKEN")
+
+
 async def main():
-    
     await init_db()
     app = Client(
         "my_account", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN, plugins=plugins
     )
     await app.start()
     await app.idle()
+
+
 asyncio.run(main())
